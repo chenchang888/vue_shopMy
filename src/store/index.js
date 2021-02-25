acbe15dae591a7cf9872a745dad81bd3bba02f10
不羁 <15256932779@163.com>
2021-02-25 08:57:44 +0800

    master

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,6 @@ import state from "./state"
 import mutations from "./mutations"
 import actions from "./actions"
 import getters from "./getters"
-<<<<<<< HEAD
-=======
-
->>>>>>> home
 Vue.use(Vuex);
 const Store = new Vuex.Store({
   state,
