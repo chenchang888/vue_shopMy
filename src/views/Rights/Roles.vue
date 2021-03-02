97432b736f83640a8df77062b0e0338adcdeb02c
不羁 <15256932779@163.com>
2021-03-02 08:52:49 +0800

    master

--- src/views/Rights/Roles.vue
+++ src/views/Rights/Roles.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>
+  <div id="roles_container">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>、
     </el-breadcrumb>
+    <el-card class="box-card">
+      <el-button type="primary">添加角色</el-button>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline class="demo-table-expand">
+              <el-form-item label="商品描述">
+                <span>{{ props.row.desc }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column label="角色名称" prop="id">
+        </el-table-column>
+        <el-table-column label="角色描述" prop="name">
+        </el-table-column>
+        <el-table-column label="操作" prop="desc">
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tableData: {}
+    }
+  },
 }
 </script>
 
 <style lang="less" scoped>
+#roles_container {
+  .box-card {
+    margin-top: 20px;
+  }
+}
 </style>
